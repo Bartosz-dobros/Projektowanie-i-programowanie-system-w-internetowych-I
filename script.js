44f843138e4c0bce2bbee0c67419365226de4478
Sigurvegarinn2000 <misiunia.10@wp.pl>
2025-06-07 17:10:55 +0200

    dodanie utworów

--- script.js
+++ script.js
@@ -1,77 +1,146 @@
 const playlists = {
   P1: {
     title: "Rap",
-    cover: "pic/playlist1.png",
+    cover: "pic/rapPlaylist.png",
     tracks: [
-      { id: "Rap1", title: "Stronger", artist: "Kanye West", music: 'music/track1.mp3'},
-      { id: "Rap2", title: "Can't Hold Us", artist: "Macklemore & Ryan Lewis", music: 'music/track1.mp3'},
-      { id: "Rap3", title: "Titanium", artist: "David Guetta ft. Sia", music: 'music/track2.mp3'}
+      { id: "Rap1", title: "Don't stop dreaming", artist: "AleksandrKoshylev", music: 'music/Rap/Dont stop dreaming - AleksandrKoshylev.mp3'},
+      { id: "Rap2", title: "Old Wisdom", artist: "AleksandrKoshylev", music: 'music/Rap/Old Wisdom - AleksandrKoshylev.mp3'}
     ]
   },
   P2: {
     title: "Jazz",
-    cover: "pic/playlist2.png",
+    cover: "pic/jazzPlaylist.png",
     tracks: [
-      { id: "Jazz1", title: "Take Five", artist: "Dave Brubeck", music: 'music/track1.mp3'},
-      { id: "Jazz2", title: "So What", artist: "Miles Davis", music: 'music/track2.mp3'},
-      { id: "Jazz3", title: "Blue in Green", artist: "Bill Evans", music: 'music/track1.mp3'},
-      { id: "Jazz4", title: "Fix You", artist: "Coldplay", music: 'music/track2.mp3'}
+      { id: "Jazz1", title: "Bossaholics-2", artist: "Nesrality", music: 'music/Jazz/Bossaholics-2 - Nesrality.mp3'},
+      { id: "Jazz2", title: "Cast", artist: "Pavlov-Music", music: 'music/Jazz/Cast - Pavlov-Music.mp3'},
+      { id: "Jazz3", title: "Espresso Chill", artist: "Captain_Sleppy", music: 'music/Jazz/Espresso Chill - Captain_Sleppy.mp3'},
+      { id: "Jazz4", title: "Happy Hour", artist: "TokyoRift", music: 'music/Jazz/Happy Hour - TokyoRift.mp3'},
+      { id: "Jazz5", title: "I'm not felling well today", artist: "0tyyfec", music: 'music/Jazz/Im not felling well today - 0tyyfec.mp3'},
+      { id: "Jazz6", title: "New Orleans", artist: "PaoloArgento", music: 'music/Jazz/New Orleans - PaoloArgento.mp3'},
+      { id: "Jazz7", title: "One Night With You", artist: "White_Records", music: 'music/Jazz/One Night With You - White_Records.mp3'},
+      { id: "Jazz8", title: "Rooftop", artist: "TokyoRifft", music: 'music/Jazz/Rooftop - TokyoRifft.mp3'},
+      { id: "Jazz9", title: "Sebuah", artist: "Kenangan", music: 'music/Jazz/Sebuah - Kenangan.mp3'},
+      { id: "Jazz10", title: "Sunset Zone", artist: "DJARTMUSIC", music: 'music/Jazz/Sunset Zone - DJARTMUSIC.mp3'}
     ]
   },
     P3: {
     title: "Pop",
-    cover: "pic/playlist3.png",
+    cover: "pic/popPlaylist.png",
     tracks: [
-      { id: "Pop1", title: "Imagine", artist: "John Lennon", music: 'music/track1.mp3'},
-      { id: "Pop2", title: "Yellow", artist: "Coldplay", music: 'music/track2.mp3'},
-      { id: "Pop3", title: "Fix You", artist: "Coldplay", music: 'music/track2.mp3'}
+      { id: "Pop1", title: "Cat Walk", artist: "Arulo", music: 'music/Pop/Cat Walk - Arulo.mp3'},
+      { id: "Pop2", title: "Classique", artist: "Mixkit", music: 'music/Pop/Classique - Mixkit.mp3'},
+      { id: "Pop3", title: "Crying Sea", artist: "Mixkit", music: 'music/Pop/Crying Sea - Mixkit-.mp3'},
+      { id: "Pop4", title: "Good Time", artist: "Grand_Project", music: 'music/Pop/Good Time - Grand_Project.mp3'},
+      { id: "Pop5", title: "Im Still Waitin", artist: "Michael Ramir C", music: 'music/Pop/Im Still Waitin - Michael Ramir C.mp3'},
+      { id: "Pop6", title: "Peenzee Otraak", artist: "Eugenio Mininni", music: 'music/Pop/Peenzee Otraak - Eugenio Mininni.mp3'},
+      { id: "Pop7", title: "Shahada", artist: "Mixkit", music: 'music/Pop/Shahada - Mixkit.mp3'},
+      { id: "Pop8", title: "Unlock Me", artist: "Prazkhanal", music: 'music/Pop/Unlock Me - Prazkhanal.mp3'},
+      { id: "Pop9", title: "Whip", artist: "Prazkhanal", music: 'music/Pop/Whip - Prazkhanal.mp3'},
+      { id: "Pop10", title: "Wind Leaves", artist: "Mixkit", music: 'music/Pop/Wind Leaves - Mixkit.mp3'}
     ]
   },
   P4: {
-    title: "Opera",
-    cover: "pic/playlist4.png",
+    title: "Classical",
+    cover: "pic/classicalPlaylist.png",
     tracks: [
-      { id: "Opera1", title: "Stronger", artist: "Kanye West", music: 'music/track1.mp3'},
-      { id: "Opera2", title: "Can't Hold Us", artist: "Macklemore & Ryan Lewis", music: 'music/track1.mp3'},
-      { id: "Opera3", title: "Titanium", artist: "David Guetta ft. Sia", music: 'music/track2.mp3'},
-      { id: "Opera4", title: "Fix You", artist: "Coldplay", music: 'music/track2.mp3'},
-      { id: "Opera5", title: "Can't Hold Us", artist: "Macklemore & Ryan Lewis", music: 'music/track1.mp3'}
+      { id: "Classical1", title: "Clair de Lune", artist: "Clude Debussy", music: 'music/Classical/Clair de Lune - Clude Debussy .mp3'},
+      { id: "Classical2", title: "In the Hall of the Mountain King", artist: "Edvard Grieg", music: 'music/Classical/In the Hall of the Mountain King - Edvard Grieg - .mp3'},
+      { id: "Classical3", title: "Moonlight Sonata", artist: "Beethoven", music: 'music/Classical/Moonlight Sonata - Beethoven.mp3'},
+      { id: "Classical4", title: "Nocturne In C Sharp Minor", artist: "Frederic Chopin", music: 'music/Classical/Nocturne In C Sharp Minor - Frederic Chopin.mp3'},
+      { id: "Classical5", title: "Overture Final", artist: "Rossini William Tell", music: 'music/Classical/Overture Final - Rossini William Tell.mp3'},
+      { id: "Classical6", title: "Pizzicato", artist: "Léo Delibes", music: 'music/Classical/Pizzicato - Léo Delibes.mp3'},
+      { id: "Classical7", title: "Sovereign", artist: "Kevin MacLeod", music: 'music/Classical/Sovereign - Kevin MacLeod .mp3'},
+      { id: "Classical8", title: "Swan Lake Op. 20, Act II No. 10", artist: "Tchaikovsky", music: 'music/Classical/Swan Lake Op. 20, Act II No. 10 - Tchaikovsky.mp3'},
+      { id: "Classical9", title: "The Nutcracker Act I, No. 2 (March)", artist: "P.I. Tchaikovsky", music: 'music/Classical/The Nutcracker Act I, No. 2 (March) - P.I. Tchaikovsky .mp3'},
+      { id: "Classical10", title: "Toccata and Fugue in D minor BWV", artist: "J.S. Bach", music: 'music/Classical/Toccata and Fugue in D minor BWV - J.S. Bach.mp3'}
     ]
   },
   P5: {
-    title: "Ballad",
-    cover: "pic/playlist5.png",
+    title: "Soul",
+    cover: "pic/soulPlaylist.png",
     tracks: [
-      { id: "Ballad1", title: "Take Five", artist: "Dave Brubeck", music: 'music/track1.mp3'},
-      { id: "Ballad2", title: "So What", artist: "Miles Davis", music: 'music/track2.mp3'},
-      { id: "Ballad3", title: "Blue in Green", artist: "Bill Evans", music: 'music/track1.mp3'},
-      { id: "Ballad4", title: "Fix You", artist: "Coldplay", music: 'music/track2.mp3'}
+      { id: "Soul1", title: "Air Flow", artist: "Magiksolo", music: 'music/Soul/Air Flow - Magiksolo.mp3'},
+      { id: "Soul2", title: "Chillout", artist: "Rockot", music: 'music/Soul/Chillout - Rockot.mp3'},
+      { id: "Soul3", title: "Desire", artist: "TurtleBeats", music: 'music/Soul/Desire - TurtleBeats.mp3'},
+      { id: "Soul4", title: "Dhik", artist: "Prazkhanal", music: 'music/Soul/Dhik - Prazkhanal.mp3'},
+      { id: "Soul5", title: "Funky Freshness", artist: "Omnitrum", music: 'music/Soul/Funky Freshness - Omnitrum.mp3'},
+      { id: "Soul6", title: "Odyssey", artist: "Magiksolo", music: 'music/Soul/Odyssey - Magiksolo.mp3'},
+      { id: "Soul7", title: "Show Me Your Soul", artist: "Rockot", music: 'music/Soul/Show Me Your Soul - Rockot.mp3'},
+      { id: "Soul8", title: "Soul of The Streets", artist: "Albert Paul", music: 'music/Soul/Soul of The Streets - Albert Paul.mp3'},
+      { id: "Soul9", title: "The Way You Linger", artist: "Alanajordan", music: 'music/Soul/The Way You Linger - Alanajordan.mp3'},
+      { id: "Soul10", title: "We Must Be Wrong", artist: "Brotheration_Records", music: 'music/Soul/We Must Be Wrong - Brotheration_Records.mp3'}
     ]
   },
     P6: {
     title: "Rock",
-    cover: "pic/playlist6.png",
+    cover: "pic/rockPlaylist.png",
     tracks: [
-      { id: "Rock1", title: "Take Five", artist: "Dave Brubeck", music: 'music/track1.mp3'},
-      { id: "Rock2", title: "So What", artist: "Miles Davis", music: 'music/track2.mp3'}
+      { id: "Rock1", title: "Action", artist: "DmitryTaras", music: 'music/Rock/Action - DmitryTaras.mp3'},
+      { id: "Rock2", title: "Chasing The Sun", artist: "TurtleBeats", music: 'music/Rock/Chasing The Sun - TurtleBeats.mp3'},
+      { id: "Rock3", title: "Firearm", artist: "AlexGrohl", music: 'music/Rock/Firearm - AlexGrohl.mp3'},
+      { id: "Rock4", title: "Kansas Blues", artist: "nickpanek620", music: 'music/Rock/Kansas Blues - nickpanek620.mp3'},
+      { id: "Rock5", title: "Ki", artist: "Lyrium", music: 'music/Rock/Ki - Lyrium.mp3'},
+      { id: "Rock6", title: "Rock Again", artist: "trtasfiq", music: 'music/Rock/Rock Again - trtasfiq.mp3'},
+      { id: "Rock7", title: "Rock Thunder", artist: "Pocketbeats", music: 'music/Rock/Rock Thunder - Pocketbeats.mp3'},
+      { id: "Rock8", title: "Survival", artist: "ikoliks", music: 'music/Rock/Survival - ikoliks.mp3'},
+      { id: "Rock9", title: "Take Me to the 80s", artist: "Cath22music", music: 'music/Rock/Take Me to the 80s - Cath22music.mp3'},
+      { id: "Rock10", title: "Whistle Joyride", artist: "Top-Flow", music: 'music/Rock/Whistle Joyride - Top-Flow.mp3'}
     ]
   },
   P7: {
     title: "Country",
-    cover: "pic/playlist7.png",
+    cover: "pic/countryPlaylist.png",
     tracks: [
-      { id: "Country1", title: "Take Five", artist: "Dave Brubeck", music: 'music/track1.mp3'},
-      { id: "Country2", title: "So What", artist: "Miles Davis", music: 'music/track2.mp3'},
-      { id: "Country3", title: "Blue in Green", artist: "Bill Evans", music: 'music/track1.mp3'},
-      { id: "Country4", title: "Fix You", artist: "Coldplay", music: 'music/track2.mp3'}
+      { id: "Country1", title: "A Night in Memphis", artist: "matthewmikemusic", music: 'music/Country/A Night in Memphis - matthewmikemusic.mp3'},
+      { id: "Country2", title: "Country Adventure", artist: "alanjordan", music: 'music/Country/Country Adventure - alanjordan.mp3'},
+      { id: "Country3", title: "Country Roads", artist: "SergePavkinMusic", music: 'music/Country/Country Roads - SergePavkinMusic.mp3'},
+      { id: "Country4", title: "Country Rock", artist: "LiteSaturation", music: 'music/Country/Country Rock - LiteSaturation.mp3'},
+      { id: "Country5", title: "Home On The Range", artist: "Tunetank", music: 'music/Country/Home On The Range - Tunetank.mp3'},
+      { id: "Country6", title: "Outlaw", artist: "Caffeine_Creek", music: 'music/Country/Outlaw - Caffeine_Creek.mp3'},
+      { id: "Country7", title: "The Lonesome Cowboy", artist: "JuliusH", music: 'music/Country/The Lonesome Cowboy 0 JuliusH.mp3'},
+      { id: "Country8", title: "Western Reflections", artist: "Sonican", music: 'music/Country/Western Reflections - Sonican.mp3'},
+      { id: "Country9", title: "What it Is", artist: "u_i6yay", music: 'music/Country/What it Is - u_i6yay.mp3'},
+      { id: "Country10", title: "Yesterday Mistakes", artist: "Alanajordan", music: 'music/Country/Yesterday Mistakes - Alanajordan.mp3'}
     ]
   },
     P8: {
-    title: "Disco",
-    cover: "pic/playlist8.png",
+    title: "EDM",
+    cover: "pic/edmPlaylist.png",
     tracks: [
-      { id: "Disco1", title: "Take Five", artist: "Dave Brubeck", music: 'music/track1.mp3'},
-      { id: "Disco2", title: "So What", artist: "Miles Davis", music: 'music/track2.mp3'}
+      { id: "EDM1", title: "Alone", artist: "BoDleasons", music: 'music/EDM/Alone - BoDleasons.mp3'},
+      { id: "EDM2", title: "English", artist: "SigmaMusicArt", music: 'music/EDM/English - SigmaMusicArt.mp3'},
+      { id: "EDM3", title: "Evil", artist: "BLACKBOX", music: 'music/EDM/Evil - BLACKBOX.mp3'},
+      { id: "EDM4", title: "Get Up", artist: "Freccero", music: 'music/EDM/Get Up - Freccero.mp3'},
+      { id: "EDM5", title: "Gorrila", artist: "Alex_MakeMusic", music: 'music/EDM/Gorrila - Alex_MakeMusic.mp3'},
+      { id: "EDM6", title: "Monika", artist: "Hasenchat", music: 'music/EDM/Monika - Hasenchat.mp3'},
+      { id: "EDM7", title: "Newbie Arriving", artist: "RibhavAgrawal", music: 'music/EDM/Newbie Arriving - RibhavAgrawal.mp3'},
+      { id: "EDM8", title: "RX FX", artist: "Oliver_Dee", music: 'music/EDM/RX FX - Oliver_Dee.mp3'},
+      { id: "EDM9", title: "Sudden Attack", artist: "BLACKBOX", music: 'music/EDM/Sudden Attack - BLACKBOX.mp3'},
+      { id: "EDM10", title: "Vertical", artist: "HVRGND", music: 'music/EDM/Vertical - HVRGND.mp3'}
+    ]
+  },
+    P9: {
+    title: "K-Pop",
+    cover: "pic/kpopPlaylist.png",
+    tracks: [
+      { id: "K-Pop1", title: "Oh Good Girl", artist: "ThePinkWitchTG", music: 'music/K-Pop/Oh Good Girl - ThePinkWitchTG.mp3'},
+      { id: "K-Pop2", title: "설레나요", artist: "옴택", music: 'music/K-Pop/설레나요 - 옴택.mp3'}
+    ]
+  },
+    P10: {
+    title: "Hip Hop",
+    cover: "pic/hiphopPlaylist.png",
+    tracks: [
+      { id: "Hip Hop1", title: "8.pta city 4", artist: "CraneHolyf", music: 'music/Hip Hop/8.pta city 4 - CraneHolyf.mp3'},
+      { id: "Hip Hop2", title: "Anxiety", artist: "Prazkhanal", music: 'music/Hip Hop/Anxiety- Prazkhanalmp3.mp3'},
+      { id: "Hip Hop3", title: "Dumb Rich", artist: "aj_bristo", music: 'music/Hip Hop/Dumb Rich- aj_bristo.mp3'},
+      { id: "Hip Hop4", title: "Dunes Distortion", artist: "Music_Unlimited", music: 'music/Hip Hop/Dunes Distortion - Music_Unlimited.mp3'},
+      { id: "Hip Hop5", title: "G Eazy NBA Type", artist: "Arulo", music: 'music/Hip Hop/G Eazy NBA Type - Arulo.mp3'},
+      { id: "Hip Hop6", title: "Good Life", artist: "FASSounds", music: 'music/Hip Hop/Good Life - FASSounds.mp3'},
+      { id: "Hip Hop7", title: "Nightmare", artist: "11dot21", music: 'music/Hip Hop/Nightmare - 11dot21.mp3'},
+      { id: "Hip Hop8", title: "Paradise Dubai", artist: "White_Records", music: 'music/Hip Hop/Paradise Dubai - White_Records.mp3'},
+      { id: "Hip Hop9", title: "Shank", artist: "LooksMusic", music: 'music/Hip Hop/Shank - LooksMusic.mp3'},
+      { id: "Hip Hop10", title: "Victory", artist: "Prazkhanal", music: 'music/Hip Hop/Victory - Prazkhanal.mp3'}
     ]
   }
 };
